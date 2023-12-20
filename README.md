0f86dc93b908c5f293d0bbc57b081cea1fb6ffd9
Congyu <leonwang998@gmail.com>
2023-12-20 21:41:52 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# Intrusive Circular Doubly Linked List in Rust
+
+Learning how to do cdlist in Rust:
+Let the elements hold the ownership, instead of the collection.
+
+## Usage
+
+See tests as examples.
+
+```rust
+#![feature(get_many_mut)]
+use cdlist::LinkNode;
+
+#[test]
+fn deref_mut() {
+    let mut node0 = LinkNode::new(1);
+    let node1 = LinkNode::new(2);
+    *node0 += *node1;
+    assert_eq!(3, *node0);
+}
+
+#[test]
+fn iter_single() {
+    let node0 = LinkNode::new(0);
+    assert_eq!(collect(&node0), vec![0]);
+}
+
+#[test]
+fn iter() {
+    let mut nodes = (0..10).map(LinkNode::new).collect::<Vec<_>>();
+    connect_all(&mut nodes, 0, 10);
+    assert_eq!(collect(&nodes[0]), (0..10).collect::<Vec<_>>());
+    assert_eq!(collect_rev(&nodes[9]), (0..10).rev().collect::<Vec<_>>());
+}
+
+#[test]
+fn iter_mut() {
+    let mut nodes = (0..10).map(LinkNode::new).collect::<Vec<_>>();
+    connect_all(&mut nodes, 0, 10);
+    let mut j = 0;
+    nodes[5].for_each_mut(|i| {
+        *i += j;
+        j += 1;
+    });
+    j = 0;
+    assert_eq!(collect(&nodes[0]), vec![5, 7, 9, 11, 13, 5, 7, 9, 11, 13]);
+    nodes[9].for_each_mut_rev(|i| {
+        *i += j;
+        j += 1;
+    });
+    assert_eq!(
+        collect(&nodes[0]),
+        vec![14, 15, 16, 17, 18, 9, 10, 11, 12, 13]
+    );
+}
+
+#[test]
+fn pop_self() {
+    let mut node0 = LinkNode::new(0);
+    node0.take();
+    assert_eq!(collect(&node0), vec![0]);
+}
+
+#[test]
+fn requeue() {
+    let mut n0 = LinkNode::new(0);
+    let mut n1 = LinkNode::new(1);
+    let mut n2 = LinkNode::new(2);
+    n0.add(&mut n1);
+    n1.add(&mut n2);
+    assert_eq!(collect(&n0), vec![0, 1, 2]);
+    n2.add(&mut n1);
+    assert_eq!(collect(&n0), vec![0, 2, 1]);
+    assert_eq!(collect(&n2), vec![2, 1, 0]);
+}
+
+#[test]
+fn take() {
+    let mut nodes = (0..10).map(LinkNode::new).collect::<Vec<_>>();
+    connect_all(&mut nodes, 0, 10);
+    assert_eq!(collect(&nodes[0]), (0..10).collect::<Vec<_>>());
+    let to_take = [0, 2, 4, 6, 8];
+    for i in to_take {
+        nodes[i].take();
+    }
+    for i in to_take {
+        assert_eq!(collect(&nodes[i]), vec![i]);
+    }
+    assert_eq!(collect(&nodes[1]), vec![1, 3, 5, 7, 9]);
+}
+
+#[test]
+fn add() {
+    let mut nodes = (0..10).map(LinkNode::new).collect::<Vec<_>>();
+    connect_all(&mut nodes, 0, 5);
+    connect_all(&mut nodes, 5, 10);
+    assert_eq!(collect(&nodes[0]), (0..5).collect::<Vec<_>>());
+    assert_eq!(collect(&nodes[5]), (5..10).collect::<Vec<_>>());
+    let (n0, n1) = nodes.split_at_mut(5);
+    n0[2].add(&mut n1[2]);
+    assert_eq!(collect(&nodes[0]), vec![0, 1, 2, 7, 3, 4]);
+    assert_eq!(collect_rev(&nodes[4]), vec![4, 3, 7, 2, 1, 0]);
+    assert_eq!(collect(&nodes[5]), vec![5, 6, 8, 9]);
+    assert_eq!(collect_rev(&nodes[9]), vec![9, 8, 6, 5]);
+}
+
+// helper functions
+
+fn collect<T: Copy>(node: &LinkNode<T>) -> Vec<T> {
+    let mut vec = vec![];
+    node.for_each(|&i| vec.push(i));
+    vec
+}
+
+fn collect_rev<T: Copy>(node: &LinkNode<T>) -> Vec<T> {
+    let mut vec = vec![];
+    node.for_each_rev(|&i| vec.push(i));
+    vec
+}
+
+fn connect_all<T>(nodes: &mut [LinkNode<T>], start: usize, end: usize) {
+    (start..(end - 1)).zip((start + 1)..end).for_each(|(i, j)| {
+        let [ni, nj] = nodes.get_many_mut([i, j]).unwrap();
+        ni.add(nj);
+    });
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,256 @@
+#![feature(offset_of)]
+#![feature(inline_const)]
+use std::{
+    marker::PhantomData,
+    mem::offset_of,
+    ops::{Deref, DerefMut},
+    pin::Pin,
+    ptr::NonNull,
+};
+
+/// Owned cell for data.
+pub struct LinkNode<T>(Pin<Box<Inner<T>>>);
+
+/// Pinned on heap for linking.
+///
+/// T can be !Unpin.
+struct Inner<T> {
+    data: T,
+    list: ListHead<T>,
+}
+
+/// Intrusive link.
+struct ListHead<T> {
+    prev: Option<NonNull<ListHead<T>>>,
+    next: Option<NonNull<ListHead<T>>>,
+    dtype: PhantomData<T>,
+}
+
+impl<T> LinkNode<T> {
+    #[inline]
+    pub fn new(data: T) -> Self {
+        let mut node = Self(Box::pin(Inner {
+            data,
+            list: ListHead {
+                prev: None,
+                next: None,
+                dtype: PhantomData,
+            },
+        }));
+        node.list_mut().init_head();
+        node
+    }
+
+    /// Pop `other` from its list and add it to `self` list.
+    #[inline]
+    pub fn add(&mut self, other: &mut LinkNode<T>) {
+        let other_list = other.list_mut();
+        unsafe {
+            other_list.delist();
+            self.list_mut().add(other_list);
+        }
+    }
+
+    /// Add `self` to `other` list.
+    #[inline]
+    pub fn add_to(&mut self, other: &mut LinkNode<T>) {
+        other.add(self)
+    }
+
+    /// Pop `self` from its current list.
+    #[inline]
+    pub fn take(&mut self) {
+        let list = self.list_mut();
+        unsafe { list.delist() };
+        list.init_head();
+    }
+
+    pub fn for_each<F>(&self, f: F)
+    where
+        F: FnMut(&T),
+    {
+        self.list().for_each(f)
+    }
+
+    pub fn for_each_mut<F>(&mut self, f: F)
+    where
+        F: FnMut(&mut T),
+    {
+        self.list_mut().for_each_mut(f)
+    }
+
+    pub fn for_each_rev<F>(&self, f: F)
+    where
+        F: FnMut(&T),
+    {
+        self.list().for_each_rev(f)
+    }
+
+    pub fn for_each_mut_rev<F>(&mut self, f: F)
+    where
+        F: FnMut(&mut T),
+    {
+        self.list_mut().for_each_rev_mut(f)
+    }
+
+    #[inline(always)]
+    fn list(&self) -> &ListHead<T> {
+        &self.0.list
+    }
+
+    #[inline(always)]
+    fn list_mut(&mut self) -> &mut ListHead<T> {
+        unsafe { &mut self.0.as_mut().get_unchecked_mut().list }
+    }
+}
+
+impl<T> DerefMut for LinkNode<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut self.0.as_mut().get_unchecked_mut().data }
+    }
+}
+
+impl<T> Deref for LinkNode<T> {
+    type Target = T;
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0.data
+    }
+}
+
+impl<T> Drop for LinkNode<T> {
+    fn drop(&mut self) {
+        unsafe { self.list_mut().delist() };
+    }
+}
+
+impl<T> ListHead<T> {
+    /// Called when node is created.
+    #[inline(always)]
+    fn init_head(&mut self) {
+        let list = NonNull::new(self as *mut ListHead<T>);
+        self.prev = list;
+        self.next = list;
+    }
+
+    /// Unlink the current node from its previous list.
+    ///
+    /// This is an incomplete operation:
+    ///
+    /// After this operation, the `prev` and `next` pointers
+    /// still points to previous linked list.
+    /// Thus, `delist()` should only be used in `drop()`
+    /// or in `add()` to update to `prev` and `next` pointers.
+    #[inline(always)]
+    unsafe fn delist(&mut self) {
+        let mut prev = self.prev.unwrap();
+        let mut next = self.next.unwrap();
+        prev.as_mut().next = Some(next);
+        next.as_mut().prev = Some(prev);
+    }
+
+    /// Add `other` between `self` and `self.next`.
+    ///
+    /// This is an incomplete operation:
+    ///
+    /// We assume that other has been `delist()` from
+    /// its previous chain, so that its previous chain
+    /// is still complete.
+    #[inline(always)]
+    unsafe fn add(&mut self, other: &mut ListHead<T>) {
+        other.prev = NonNull::new(self as *mut ListHead<T>);
+        other.next = self.next;
+        self.next.unwrap().as_mut().prev = NonNull::new(other as *mut ListHead<T>);
+        self.next = NonNull::new(other as *mut ListHead<T>);
+    }
+
+    #[inline(always)]
+    fn for_each<F>(&self, mut f: F)
+    where
+        F: FnMut(&T),
+    {
+        let mut this = self;
+        loop {
+            f(this.get());
+            let next = this.next.unwrap();
+            if next.as_ptr() as *const ListHead<T> == self as *const ListHead<T> {
+                break;
+            }
+            this = unsafe { next.as_ref() };
+        }
+    }
+
+    #[inline(always)]
+    fn for_each_mut<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&mut T),
+    {
+        let mut this = &mut *self;
+        loop {
+            f(this.get_mut());
+            let mut next = this.next.unwrap();
+            if next.as_ptr() as *const ListHead<T> == self as *const ListHead<T> {
+                break;
+            }
+            this = unsafe { next.as_mut() };
+        }
+    }
+
+    #[inline(always)]
+    fn for_each_rev<F>(&self, mut f: F)
+    where
+        F: FnMut(&T),
+    {
+        let mut this = self;
+        loop {
+            f(this.get());
+            let prev = this.prev.unwrap();
+            if prev.as_ptr() as *const ListHead<T> == self as *const ListHead<T> {
+                break;
+            }
+            this = unsafe { prev.as_ref() };
+        }
+    }
+
+    #[inline(always)]
+    fn for_each_rev_mut<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&mut T),
+    {
+        let mut this = &mut *self;
+        loop {
+            f(this.get_mut());
+            let mut prev = this.prev.unwrap();
+            if prev.as_ptr() as *const ListHead<T> == self as *const ListHead<T> {
+                break;
+            }
+            this = unsafe { prev.as_mut() };
+        }
+    }
+
+    #[inline(always)]
+    fn get(&self) -> &T {
+        &unsafe { self.inner() }.data
+    }
+
+    #[inline(always)]
+    fn get_mut(&mut self) -> &mut T {
+        &mut unsafe { self.inner_mut() }.data
+    }
+
+    #[inline(always)]
+    unsafe fn inner(&self) -> &Inner<T> {
+        &*((self as *const Self as *const char).offset(Self::offset()) as *const Inner<T>)
+    }
+
+    #[inline(always)]
+    unsafe fn inner_mut(&mut self) -> &mut Inner<T> {
+        &mut *((self as *mut Self as *mut char).offset(Self::offset()) as *mut Inner<T>)
+    }
+
+    #[inline(always)]
+    const fn offset() -> isize {
+        const { -(offset_of!(Inner<T>, list) as isize) }
+    }
+}
